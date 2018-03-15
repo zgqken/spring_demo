60dd3d10aa50e1c866b0a3df6773373108c31de9
卓国铨 <zgqken@qq.com>
2018-03-15 16:36:31 +0800

    添加定时任务+线程池

--- src/main/java/com/ken/main/DemoApplication.java
+++ src/main/java/com/ken/main/DemoApplication.java
@@ -3,9 +3,14 @@ package com.ken.main;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan("com.ken")
 public class DemoApplication extends SpringBootServletInitializer {
 
@@ -14,4 +19,10 @@ public class DemoApplication extends SpringBootServletInitializer {
         return application.sources(DemoApplication.class);
     }
 
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(5);
+        return taskScheduler;
+    }
 }
---
+++ src/main/java/com/ken/service/ScheduledTest.java
@@ -0,0 +1,30 @@
+package com.ken.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zgquan(108563)
+ * @date 2018/3/15 0015
+ * @since
+ */
+@Component
+public class ScheduledTest {
+
+
+    @Scheduled(fixedDelay = 5000)
+    public void test1(){
+        System.out.println("test1");
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void test2(){
+        System.out.println("test2");
+    }
+
+}
