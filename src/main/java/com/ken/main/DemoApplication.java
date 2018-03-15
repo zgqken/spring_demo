908b2be099a81982e95dab48f0cb80f1ce817fe9
卓国铨 <zgqken@qq.com>
2018-03-15 15:11:36 +0800

    update

--- src/main/java/com/ken/main/DemoApplication.java
+++ src/main/java/com/ken/main/DemoApplication.java
@@ -1,13 +1,17 @@
 package com.ken.main;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.ken")
-public class DemoApplication {
+@ComponentScan("com.ken")
+public class DemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
 }
