19e1f7d2628eb49ac9dba3f2dc97c9cf1c9fffb4
zgqken <zgqken@qq.com>
2018-04-25 21:27:18 +0800

    update

---
+++ src/test/java/com/ken/java8/LambdaTest.java
@@ -0,0 +1,43 @@
+package com.ken.java8;
+
+import lombok.Data;
+import org.junit.Test;
+
+/**
+ * @author：davy(888032)
+ * @date：2018/4/1
+ */
+public class LambdaTest {
+
+
+    @Test
+    public void filterMap(){
+
+
+
+
+    }
+
+
+
+
+}
+
+
+@Data
+class User{
+    private Integer id;
+
+    private String userName;
+
+    private String password;
+
+}
+
+@Data
+class Password{
+    private Integer id;
+
+    private String password;
+
+}
